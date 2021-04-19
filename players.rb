e5fa3df73eeb03d3a155644e6b02e36f1f00ff02
Dmitry Gorodnichy <dgo@jetthoughts.com>
2021-04-19 14:55:57 +0300

    Fixed incorrect variables names

--- players.rb
+++ players.rb
@@ -14,7 +14,7 @@ class Players
 
   def generate_statistics(data)
     statistics = []
-    players = data.group_by { |a| [a[:player], a[:year]] }
+    players = data.group_by { |row| [row[:player], row[:year]] }
 
     players.each do |player_year, attributes|
       player, year = player_year
@@ -25,9 +25,9 @@ class Players
   end
 
   def calculate_user_statistics(player, year, attributes)
-    average_hits = attributes.map { |a| a[:H].to_f }.inject(:+)
-    average_at_bats = attributes.map { |a| a[:AB].to_f }.inject(:+)
-    batting_average = (average_hits / average_at_bats).round(3)
+    total_hits = attributes.map { |a| a[:H].to_f }.inject(:+)
+    total_at_bats = attributes.map { |a| a[:AB].to_f }.inject(:+)
+    batting_average = (total_hits / total_at_bats).round(3)
 
     {
       player: player,
